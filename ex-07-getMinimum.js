6da14a9cb9d4349598d5dc5aa2e382594a345b82
nstreetman <nicole.streetman@Nicoles-MacBook-Air.local>
2017-01-23 17:44:16 -0500

    first 6 2nd shot

--- ex-07-getMinimum.js
+++ ex-07-getMinimum.js
@@ -1,14 +1,18 @@
 
 // Part 07 : getMinimum()
 
-// Write a function cthat will find the minimum of four 
+// Write a function cthat will find the minimum of four
 // input numbers. You can do it using nested if statements,
 // boolean operators, or both (but not neither).
 
+function getMinimum(number1,number2,number3,number4){
+	if
+   return number1
+}
 
 //*-*~*~*~*~*~ Don't Touch *~*~*~*~*~*~*~*
 
 console.assert( getMinimum(1,2,4,0) === 0 )
 console.assert( getMinimum(1000,-2,-99,50) === -99 )
 console.assert( getMinimum(-1000,-2,-99,50) === -1000 )
-console.assert( getMinimum(400,-11,12,-5) === -11 )	
+console.assert( getMinimum(400,-11,12,-5) === -11 )
